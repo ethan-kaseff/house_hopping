f150ee50e6f73fe2b740a615935f2fe6e71a3c29
Ethan Kaseff <ethan@kaseff.com>
2021-06-23 15:42:51 -0500

    now getting started

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -1,100 +1,83 @@
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom'
 
-import Moment from "moment";
-import { extendMoment } from "moment-range";
 
 import { DateRangePicker } from 'react-dates';
+import DataListInput from 'react-datalist-input';
+
 import 'react-dates/lib/css/_datepicker.css';
+import './HomePage.css';
 
-import { getPlanes } from '../../store/plane';
-import { createBooking } from '../../store/booking';
-import { updateCurrentBookings } from '../../store/booking';
+import { getAvailablePlanes } from '../../store/plane';
+import { saveCurrentDates } from '../../store/booking';
 
 
-import './PlaneProfile.css';
-
-function PlaneProfile() {
+function HomePage({ isLoaded }) {
     const dispatch = useDispatch();
-    const { planeId } = useParams();
+    const history = useHistory();
 
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [focusedInput, setfocusedInput] = useState(null);
 
-    useEffect(() => {
-        dispatch(getPlanes());
-        dispatch(updateCurrentBookings(planeId))
-    }, [dispatch, planeId])
+    // For the DataList
+    const [state, setState] = useState();
 
-    const plane = useSelector(state => state.plane[planeId]);
-    const user = useSelector(state => state.session.user);
-    const dates = useSelector(state => state.booking);
-    const currentBookings = useSelector(state => state.booking.currentBookings);
-
-    const currentBookingsArr = [];
-    for (const key in currentBookings) {
-        currentBookingsArr.push(currentBookings[key])
-    }
-
-    const isBlocked = date => {
-        let bookedRanges = [];
-        let blocked;
-        currentBookingsArr.map(booking => {
-            return bookedRanges = [...bookedRanges,
-            Moment.range(booking.startDate, booking.endDate)]
-        }
-        );
-        blocked = bookedRanges.find(range => range.contains(date));
-        return blocked;
-    };
-
-
-
-    useEffect(() => {
-        if (dates) {
-            setStartDate(dates.startDate);
-            setEndDate(dates.endDate);
-        }
-
-    }, [dates])
-
-    const bookIt = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createBooking(user.id, planeId, startDate, endDate));
-        console.log('something')
+
+        dispatch(saveCurrentDates(startDate, endDate))
+
+        dispatch(getAvailablePlanes(startDate, endDate))
+
+        history.push(`/searchResults`);
     }
 
+    const items = [
+        {
+            key: 1,
+            label: 'Kansas'
+        },
+        {
+            key: 2,
+            label: 'Missouri'
+        },
+        {
+            key: 3,
+            label: 'California'
+        },
+        {
+            key: 4,
+            label: 'Wisconsin'
+        },
+    ]
+
+    const onSelect = useCallback((selectedItem) => {
+        setState(selectedItem);
+    })
 
     return (
         <>
-            <div className='nav-organizer'></div>
-            {plane &&
-                <div className='profile-content'>
-                    <div></div>
-                    <div className='plane-info'>
-                        <img src={plane.imageLink} alt='plane' className='image'></img>
-                        <h1>{plane.name}</h1>
-                        <label>Category: </label>
-                        <span>{plane.categoryId}</span>
-                        <h3>Description</h3>
-                        <p>{plane.description}</p>
-                        <h3>Plane Facts</h3>
-                        <div className='plane-facts'>
-                            <label>Refill Miles: </label>
-                            <span>{plane.refillMiles}</span>
-                            <br></br>
-                            <label>Year Built: </label>
-                            <span>{plane.yearBuilt}</span>
-                            <br></br>
-                            <label>Zip Code Location: </label>
-                            <span>{plane.zipCode}</span>
+            <div className='home-page-main-area'>
+                <div className='nav-organizer'></div>
+                <div className='photo-box'>
+                    <div className='mottoo'>
+                        <div></div>
+                        <div>
+                            <h1>Experience the skies in Fli-Res</h1>
                         </div>
                     </div>
-                    <div className='booking-space'>
-                        <div className='datepicker'>
-                            <form onSubmit={bookIt}>
+                    <div className='search-bar-container'>
+                        <form onSubmit={handleSubmit} className='search-bar'>
+                            <div className='autocomplete-div'>
+                                <DataListInput
+                                    placeholder="Select a State..."
+                                    items={items}
+                                    onSelect={onSelect}
+                                />
+                            </div>
+                            <div>
                                 <DateRangePicker
                                     startDate={startDate} // momentPropTypes.momentObj or null,
                                     startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
@@ -107,20 +90,33 @@ function PlaneProfile() {
                                     focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                                     onFocusChange={focusedInput => setfocusedInput(focusedInput)} // PropTypes.func.isRequired,
                                     showDefaultInputIcon
-                                // isDayBlocked={isBlocked}
                                 />
-                                <button type='submit'>Book</button>
-
-                            </form>
-                        </div>
+                            </div>
+                            <div>
+                                <button type='submit' id='search-submit'>Submit</button>
+                            </div>
+                        </form>
                     </div>
-                    <div></div>
+
                 </div>
-            }
+            </div>
+            <div className='categories-area'>
+                <h2>Categories</h2>
+                <div className='category-section'>
+                    <h4>4k</h4>
+                    <p>The best planes with unending service</p>
+                </div>
+                <div className='category-section'>
+                    <h4>1080p</h4>
+                    <p>The best planes with unending service</p>
+                </div>
+                <div className='category-section'>
+                    <h4>720p</h4>
+                    <p>Getting everyone from point A to B </p>
+                </div>
+            </div>
         </>
     )
-
 }
 
-
-export default PlaneProfile
+export default HomePage;
